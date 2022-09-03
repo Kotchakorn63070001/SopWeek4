403ffc0094fbb9cf080bc207c491471104c47694
Kotchakorn63070001 <63070001@kmitl.ac.th>
2022-09-04 02:26:03 +0700

    edit CashierView

--- src/main/java/com/example/week4/CashierView.java
+++ src/main/java/com/example/week4/CashierView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Route(value = "/index2")
 public class CashierView extends VerticalLayout {
@@ -33,5 +34,22 @@ public class CashierView extends VerticalLayout {
         b1.setPrefixComponent(new Div(new Text("$1:")));
         add(change, btnCompute, b1000, b500, b100, b20, b10, b5, b1);
 
+        btnCompute.addClickListener(event -> {
+            Integer money = Integer.parseInt(change.getValue());
+
+            Change out =  WebClient.create()
+                    .get()
+                    .uri("http://localhost:8080/getChange/"+money)
+                    .retrieve()
+                    .bodyToMono(Change.class)
+                    .block();
+                    b1000.setValue(String.valueOf(out.getB1000()));
+                    b500.setValue(String.valueOf(out.getB500()));
+                    b100.setValue(String.valueOf(out.getB100()));
+                    b20.setValue(String.valueOf(out.getB20()));
+                    b10.setValue(String.valueOf(out.getB10()));
+                    b5.setValue(String.valueOf(out.getB5()));
+                    b1.setValue(String.valueOf(out.getB1()));
+        });
     }
 }
